14f0ffaccafc32104f980aeec18cea798fc90207
Chad Edwards <chadedwardsofficial@gmail.com>
2023-12-07 18:29:46 -0600

    debugged

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -40,37 +40,31 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+
 // create new product
-router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
+router.post('/', async (req, res) => {
+  try {
+    // Check if the provided category_id exists in the Category table
+    const categoryExists = await Category.findByPk(req.body.category_id);
+    if (!categoryExists) {
+      return res.status(400).json({ error: 'Invalid category_id' });
     }
-  */
-  Product.create(req.body)
-    .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
-          return {
-            product_id: product.id,
-            tag_id,
-          };
-        });
-        return ProductTag.bulkCreate(productTagIdArr);
-      }
-      // if no product tags, just respond
-      res.status(200).json(product);
-    })
-    .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
+console.log(req.body);
+    // Create the product
+    const createdProduct = await Product.create({
+      product_name: req.body.product_name,
+      price: req.body.price,
+      stock: req.body.stock,
+      category_id: req.body.category_id,
     });
+
+    res.status(201).json(createdProduct);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ error: 'Failed to create product' });
+  }
 });
+
 
 // update product
 router.put('/:id', (req, res) => {
